b6aad0f3ec6963f6c381b8b8601f4a26ff1b98f3
liuyi <ly13858247571@gmail.com>
2025-05-19 22:12:56 +0800

    finish problem1

--- ps1/ps1/src/twitter/Extract.java
+++ ps1/ps1/src/twitter/Extract.java
@@ -3,12 +3,16 @@
  */
 package twitter;
 
+import java.time.Instant;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Extract consists of methods that extract information from a list of tweets.
- * 
+ *
  * DO NOT change the method signatures and specifications of these methods, but
  * you should implement their method bodies, and you may add new public or
  * private methods or classes if you like.
@@ -17,19 +21,26 @@ public class Extract {
 
     /**
      * Get the time period spanned by tweets.
-     * 
+     *
      * @param tweets
      *            list of tweets with distinct ids, not modified by this method.
      * @return a minimum-length time interval that contains the timestamp of
      *         every tweet in the list.
      */
     public static Timespan getTimespan(List<Tweet> tweets) {
-        throw new RuntimeException("not implemented");
+        Instant begin=tweets.get(0).getTimestamp(),end=tweets.get(0).getTimestamp();
+        for(Tweet tweet:tweets){
+            if(tweet.getTimestamp().compareTo(begin)<0)
+                begin=tweet.getTimestamp();
+            if(tweet.getTimestamp().compareTo(end)>0)
+                end=tweet.getTimestamp();
+        }
+        return new Timespan(begin,end);
     }
 
     /**
      * Get usernames mentioned in a list of tweets.
-     * 
+     *
      * @param tweets
      *            list of tweets with distinct ids, not modified by this method.
      * @return the set of usernames who are mentioned in the text of the tweets.
@@ -37,13 +48,21 @@ public class Extract {
      *         defined by Tweet.getAuthor()'s spec).
      *         The username-mention cannot be immediately preceded or followed by any
      *         character valid in a Twitter username.
-     *         For this reason, an email address like bitdiddle@mit.edu does NOT 
+     *         For this reason, an email address like bitdsiddle@mit.edu does NOT
      *         contain a mention of the username mit.
      *         Twitter usernames are case-insensitive, and the returned set may
      *         include a username at most once.
      */
     public static Set<String> getMentionedUsers(List<Tweet> tweets) {
-        throw new RuntimeException("not implemented");
+        Set<String> userSet=new HashSet<>();
+        Pattern mentionPattern = Pattern.compile("(?<![A-Za-z0-9_-])@([A-Za-z0-9_-]{1,15})(?![A-Za-z0-9_-])");
+        for(Tweet tweet:tweets){
+            Matcher matcher=mentionPattern.matcher(tweet.getText());
+            while(matcher.find()){
+                userSet.add(matcher.group(1).toLowerCase());
+            }
+        }
+        return userSet;
     }
 
 }
--- ps1/ps1/test/twitter/ExtractTest.java
+++ ps1/ps1/test/twitter/ExtractTest.java
@@ -3,45 +3,124 @@
  */
 package twitter;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ExtractTest {
 
     /*
-     * TODO: your testing strategies for these methods should go here.
+     *test getTimespan:
+     * partition:
+     * item_num  :1,2,5
+     * item_order:equal,partly equal,<,>,mixture
+     *
+     * test getMentionedUsername:
+     * partition:
+     * ite_num   :1,2
+     * name_count:0,1,4
+     * name_size :0(""),5,15(boundary),16(overflow)
+     * name_dupic:no dupicated name,has duplicated name(different int capital)
+     * capital   :all lowercase,all uppercase,mixture
      * See the ic03-testing exercise for examples of what a testing strategy comment looks like.
      * Make sure you have partitions.
      */
-    
+
     private static final Instant d1 = Instant.parse("2016-02-17T10:00:00Z");
     private static final Instant d2 = Instant.parse("2016-02-17T11:00:00Z");
-    
+    private static final Instant d3 = Instant.parse("2017-02-17T10:00:00Z");
+    private static final Instant d4 = Instant.parse("2018-02-17T11:00:00Z");
+    private static final Instant d5 = Instant.parse("2019-02-17T10:00:00Z");
+    private static final Instant d6 = Instant.parse("2016-02-17T11:00:00Z");
+
     private static final Tweet tweet1 = new Tweet(1, "alyssa", "is it reasonable to talk about rivest so much?", d1);
+    private static final Tweet tweet3 = new Tweet(1, "alyssa", "is it reas @liuyi @1234567890123456 rivest so much?", d3);
+    private static final Tweet tweet4 = new Tweet(1, "alyssa", "is it rea@ to talk ab @123456789012345 rivest so much?", d4);
+    private static final Tweet tweet5 = new Tweet(1, "alyssa", "is it rea @hello @HelLo to@fsfj talk about rivest so much?", d5);
+    private static final Tweet tweet6 = new Tweet(1, "alyssa", "is it reasonable to talk about rivest so much?", d1);
+
+
     private static final Tweet tweet2 = new Tweet(2, "bbitdiddle", "rivest talk in 30 minutes #hype", d2);
-    
+
+    @Test
+    public void testGetMentionedUsersTwoTweetsWithNoName() {
+        Set<String> userNameSet= Extract.getMentionedUsers(Arrays.asList(tweet1, tweet2));
+        assertEquals("expect zero set size", 0, userNameSet.size());
+    }
+    @Test
+    public void testGetMentionedUsersOneTweetsWithOneOverflowAndOneValidName() {
+        Set<String> userNameSet= Extract.getMentionedUsers(List.of( tweet3));
+        assertEquals("expect one set size", 1, userNameSet.size());
+        Set<String> userNameSetCompare=new HashSet<>();
+        userNameSetCompare.add("liuyi");
+        assertEquals("expect one userName: liuyi", userNameSetCompare, userNameSet);
+    }
+
+    @Test
+    public void testGetMentionedUsersWithBoundaryAndDuplicatedAndValidAndEmptyName() {
+        Set<String> userNameSet= Extract.getMentionedUsers(Arrays.asList(tweet5, tweet4));
+        assertEquals("expect one set size", 2, userNameSet.size());
+        Set<String> userNameSetCompare=new HashSet<>();
+        userNameSetCompare.add("hello");
+        userNameSetCompare.add("123456789012345");
+        assertEquals("expect one userName: hello,123456789012345", userNameSetCompare, userNameSet);
+    }
+
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
-    
+
     @Test
     public void testGetTimespanTwoTweets() {
         Timespan timespan = Extract.getTimespan(Arrays.asList(tweet1, tweet2));
-        
+
         assertEquals("expected start", d1, timespan.getStart());
         assertEquals("expected end", d2, timespan.getEnd());
     }
-    
+
+    @Test
+    public void testGetTimespanOneTweets() {
+        Timespan timespan = Extract.getTimespan(List.of(tweet1));
+        assertEquals("expected start", d1, timespan.getStart());
+        assertEquals("expected end", d1, timespan.getEnd());
+    }
+    @Test
+    public void testGetTimespanDuplicatedTweets() {
+        Timespan timespan = Extract.getTimespan(Arrays.asList(tweet1,tweet6));
+        assertEquals("expected start", d1, timespan.getStart());
+        assertEquals("expected end", d1, timespan.getEnd());
+    }
+    @Test
+    public void testGetTimespanAscendingTweets() {
+        Timespan timespan = Extract.getTimespan(Arrays.asList(tweet1,tweet2,tweet3,tweet4));
+        assertEquals("expected start", d1, timespan.getStart());
+        assertEquals("expected end", d4, timespan.getEnd());
+    }
+    @Test
+    public void testGetTimespanDescendingTweets() {
+        Timespan timespan = Extract.getTimespan(Arrays.asList(tweet4,tweet3,tweet2,tweet1));
+        assertEquals("expected start", d1, timespan.getStart());
+        assertEquals("expected end", d4, timespan.getEnd());
+    }
+    @Test
+    public void testGetTimespanMixtureOrderTweets() {
+        Timespan timespan = Extract.getTimespan(Arrays.asList(tweet2,tweet3,tweet4,tweet1));
+        assertEquals("expected start", d1, timespan.getStart());
+        assertEquals("expected end", d4, timespan.getEnd());
+    }
+
     @Test
     public void testGetMentionedUsersNoMention() {
         Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet1));
-        
+
         assertTrue("expected empty set", mentionedUsers.isEmpty());
     }
 
@@ -51,7 +130,7 @@ public class ExtractTest {
      * implementations of Extract, which will be done by overwriting
      * (temporarily) your version of Extract with the staff's version.
      * DO NOT strengthen the spec of Extract or its methods.
-     * 
+     *
      * In particular, your test cases must not call helper methods of your own
      * that you have put in Extract, because that means you're testing a
      * stronger spec than Extract says. If you need such helper methods, define
